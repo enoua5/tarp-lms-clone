0ec7698de8c05ad446645afdd2f0193c33992803
Artem <arty.nar00@gmail.com>
2022-09-20 20:03:35 -0600

    Added superuser admin password

--- authentication/forms.py
+++ authentication/forms.py
@@ -5,6 +5,18 @@ from django.contrib.auth import get_user_model
 class RegistrationForm(UserCreationForm):
     birthday=forms.DateField(label='birthday')
 
+    # def clean(self):
+    #     cd = self.cleaned_data
+
+    #     password1 = cd.get("password1")
+    #     password2 = cd.get("password2")
+
+    #     if password1 != password2:
+    #         #Or you might want to tie this validation to the password1 field
+    #         raise ValidationError("Passwords did not match")
+
+
+    #return cd
     class Meta:
         model = get_user_model() 
         
