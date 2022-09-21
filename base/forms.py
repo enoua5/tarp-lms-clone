685ca593e5103874d30a934cf753095f50caef24
Daniel Oldham <danieloldham@mail.weber.edu>
2022-09-21 10:59:06 -0600

    Added bootstrap styles to the image upload functionality

--- base/forms.py
+++ base/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import TextInput, FileInput
 from .models import Image
 
 
@@ -6,3 +7,14 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Image
         fields = ('title', 'image')
+        # widgets are required to give form components Bootstrap classes for styling
+        widgets = {
+            'title': TextInput(attrs={
+                'class': "form-control",
+                'type': "text",
+            }),
+            'image': FileInput(attrs={
+                'class': "form-control",
+                'type': "file",
+            }),
+        }
