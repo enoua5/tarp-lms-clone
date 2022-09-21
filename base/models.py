098187800d7b49faba239089debb0ca6c4d50e4d
Daniel Oldham <danieloldham@mail.weber.edu>
2022-09-21 11:50:15 -0600

    Commented models.py

--- base/models.py
+++ base/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 # Create your models here.
+
+# This will be a user model in the future
 class Image(models.Model):
     title = models.CharField(max_length=200)
     # uploads to MEDIA_ROOT/images
