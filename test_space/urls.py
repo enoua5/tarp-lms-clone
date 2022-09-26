0a952ef6ce0210bf0a7eb36ce10e35239f935173
Jacob Allen <enoua5@gmail.com>
2022-09-26 12:46:03 -0600

    Refactor test url

---
+++ test_space/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+# App Name
+app_name = 'test'
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('template', views.template, name="Template Loading"),
+    path('profile', views.profile, name="profile")
+]
---
+++ test_space/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from . import urls
+
+# Create your views here.
+
+def index(req):
+    ctx = {
+        'urls': urls.urlpatterns
+    }
+    print(ctx["urls"][0].name)
+    return render(req, 'tests/index.html', ctx)
+
+def template(req):
+    return render(req, 'tests/template.html')
+
+def profile(req):
+    return render(req, 'tests/profile.html')
