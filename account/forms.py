f51c63b0457b87a2b8b383271b2dad4130171e4a
Daniel Oldham <danieloldham@mail.weber.edu>
2022-09-26 20:01:22 -0600

    Set update profile form elements to not required

--- account/forms.py
+++ account/forms.py
@@ -14,11 +14,11 @@ class UpdateUserForm(forms.ModelForm):
 
 class UpdateProfileForm(forms.ModelForm):
     image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control', 'type': 'file'}), required=False)
-    bio = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
-    address = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    link1 = forms.URLField(max_length=200, widget=forms.URLInput(attrs={'class': 'form-control'}))
-    link2 = forms.URLField(max_length=200, widget=forms.URLInput(attrs={'class': 'form-control'}))
-    link3 = forms.URLField(max_length=200, widget=forms.URLInput(attrs={'class': 'form-control'}))
+    bio = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}), required=False)
+    address = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
+    link1 = forms.URLField(max_length=200, widget=forms.URLInput(attrs={'class': 'form-control'}), required=False)
+    link2 = forms.URLField(max_length=200, widget=forms.URLInput(attrs={'class': 'form-control'}), required=False)
+    link3 = forms.URLField(max_length=200, widget=forms.URLInput(attrs={'class': 'form-control'}), required=False)
 
     class Meta:
         model = Profile
