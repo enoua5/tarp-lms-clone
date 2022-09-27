9450c481c4c042c9103d58ace7bbfce4928d7cb8
Daniel Oldham <danieloldham@mail.weber.edu>
2022-09-26 19:57:02 -0600

    Fixed(?) problem where User doesn't have a profile record and accesses profile page

--- account/views.py
+++ account/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import UpdateUserForm, UpdateProfileForm
+from .models import Profile
 
 
 # Create your views here.
 @login_required
 def profile(request):
+    # janky way to make sure there is actually a profile
+    if not Profile.objects.filter(user=request.user).exists():
+        new_profile = Profile(user=request.user)
+        new_profile.save()
+
     return render(request, 'account/profile.html', {})
 
 
 @login_required
 def edit(request):
+    # janky way to make sure there is actually a profile
+    if not Profile.objects.filter(user=request.user).exists():
+        new_profile = Profile(user=request.user)
+        new_profile.save()
+
     if request.method == 'POST':
         user_form = UpdateUserForm(request.POST, instance=request.user)
         profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
