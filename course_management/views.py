1c9bc1d9ab04cd151a6fd430a206eeeaacc76347
Artem <arty.nar00@gmail.com>
2022-09-28 21:00:44 -0600

    Changed the template

--- course_management/views.py
+++ course_management/views.py
@@ -13,4 +13,11 @@ def course_management(request):
         'course_list': course_list,
     }
 
-    return render(request, 'course_management/course-management.html', context)
+    # Step 1: Get list of courses that are under the currently authenticated instructor
+    try:        
+        course_list = Course.objects.get(instructor=request.user)
+        return render(request, 'course_management/course-management.html', {'course_list' : course_list})
+    except:
+        # This will run if the currently logged in user doesn't have any courses, or isn't logged in.
+        return render(request, 'course_management/course-management.html', {})
+    
