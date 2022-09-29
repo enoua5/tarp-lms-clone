dadb2a194d7e1ac3e5191cac5e9dbcbb6e4db60a
Artem <arty.nar00@gmail.com>
2022-09-28 20:28:29 -0600

    Changed the templates

--- course_management/views.py
+++ course_management/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import Group
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
+from .models import Course
 
 # Create your views here.
 def course_management(request):
-    context = {}
+
+    current_user = request.user
+    course_list = Course.objects.all()
+
+    course_list = {}
+    context = {
+        'course_list': course_list,
+    }
+
     return render(request, 'course_management/course-management.html', context)
