137658468c315474f33022cd3336beae74757563
Artem <arty.nar00@gmail.com>
2022-10-03 16:44:04 -0600

    Finished the styling of the form

--- course_management/forms.py
+++ course_management/forms.py
@@ -16,16 +16,22 @@ class CourseForm(ModelForm):
     class Meta:
         model = Course 
         exclude = ['instructor']
+
+        # We can change the labels for anything!
         labels = {
         "course_num":  "Course number"
         }
 
+        # This is where the pretty magic comes in!!
         widgets = {
-            "department": forms.TextInput(attrs={'placeholder': 'Name', 'class': 'form-control'}),
-            "course_num": forms.NumberInput(attrs={'placeholder': 0, 'class': 'form-control'}),
-            "course_name": forms.TextInput(attrs={'placeholder': 'Course Name', 'class': 'form-control'}),
+            "department": forms.TextInput(attrs={'placeholder': 'Department', 'id':"department", 'class': 'form-control'}),
+            "course_num": forms.NumberInput(attrs={'id':"course_num",'placeholder': 0, 'class': 'form-control'}),
+            "course_name": forms.TextInput(attrs={'id':"course_name",'placeholder': 'Course Name', 'class': 'form-control'}),
             "meeting_days": forms.CheckboxSelectMultiple(choices=DAYS_OF_WEEK),
-            "meeting_start_time": forms.TimeInput(attrs={'type': 'time', 'class': 'form-control'})
+            "meeting_start_time": forms.TimeInput(attrs={'id':"meeting_start_time",'type': 'time', 'class': 'form-control'}),
+            "meeting_end_time": forms.TimeInput(attrs={'id':"meeting_end_time",'type': 'time', 'class': 'form-control'}),
+            "meeting_location": forms.TextInput(attrs={'id':"meeting_location",'placeholder': 'TBA', 'class': 'form-control'}),
+            "credit_hours":forms.NumberInput(attrs={'id':"credit_hours",'class': 'form-control'})
         }
 
         error_messages = {
@@ -34,11 +40,7 @@ class CourseForm(ModelForm):
            },
         }
 
-        attrs = {
-        'course_num': {
-            'class': "form-control",
-           },
-        }
+        # Just to remember the fields
         # fields = ["course_num", "department", "course_name", 
         #           "meeting_days", "meeting_start_time", "instructor",
         #           "meeting_end_time", "meeting_location", "credit_hours"]
--- course_management/models.py
+++ course_management/models.py
@@ -20,7 +20,7 @@ YEAR_IN_SCHOOL_CHOICES = [
 
 class Course(models.Model):
     department = models.CharField(max_length=20)
-    course_num = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(10000)])
+    course_num = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(10000)])
     course_name = models.CharField(max_length=100)
     instructor = models.ForeignKey(
         settings.AUTH_USER_MODEL,
@@ -31,8 +31,8 @@ class Course(models.Model):
 
     meeting_start_time = models.TimeField(default='12:00')
     meeting_end_time = models.TimeField(default='12:00')
-    meeting_location = models.CharField(max_length=25, default="TBA")
-    credit_hours = models.PositiveSmallIntegerField(default=1)
+    meeting_location = models.CharField(max_length=25)
+    credit_hours = models.PositiveSmallIntegerField(default=3)
 
     def __str__(self):
         return self.department + " " + str(self.course_num) + " " + self.course_name
