60a090091cd8a8f9cfa666e319f6c8d48d63c9e8
Griffin <GriffinGould@mail.weber.edu>
2022-10-04 19:39:15 -0600

    Added a test label

--- calendars/views.py
+++ calendars/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from course_management.models import Course
+from json import dumps
 
 # Create your views here.
 def displaycalendar(request):
-    return render(request, 'calendars/calendar.html', {'page_title': 'Calendar'})
+    courses_list = Course.objects.all()
+
+    return render(request, 'calendars/calendar.html', {'course_list' : courses_list, 'page_title': 'Calendar'})
