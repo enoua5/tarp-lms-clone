d4dbcc63e854f1f6d31109d567ce0e9da447b2b4
Jacob Allen <enoua5@gmail.com>
2022-10-11 00:53:11 -0600

    added get_mine command

--- data/views.py
+++ data/views.py
@@ -1,9 +1,11 @@
+""" See `QUERY_COMMANDS` to find a list of accepted commands and the functions that handle them. """
+
 from datetime import datetime, time, date
+from http import HTTPStatus
 from json import dumps, JSONEncoder
 from django.forms.models import model_to_dict
-from django.db.models import Model
+from django.db.models import Model, Q
 from django.db.models.fields.files import ImageFieldFile
-from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -28,30 +30,44 @@ def queryset_to_list(qs, fields=None, exclude=None):
         arr.append(model_to_dict(i, fields=fields, exclude=exclude))
     return arr
 
+def ItemQueryDesc(model, permission, only_mine=True, user_fields=[], user_lists=[]):
+    return {
+        'model': model,
+        'permission': permission,
+        'only_mine': only_mine,
+        'user_fields': user_fields,
+        'user_lists': user_lists,
+    }
+
 ITEM_MODELS = {
-    'course': {'model':Course, 'permission': "course_management.view_course"},
-    'assignment': {'model':Assignment, 'permission': "course_management.view_assignment"},
-    'profile': {'model':Profile, 'permission': "course_management.view_profile"},
+    'course': ItemQueryDesc(Course, "course_management.view_course", only_mine=False, user_lists=["students"], user_fields=["instructor_id"]),
+    'assignment': ItemQueryDesc(Assignment, "course_management.view_assignment", user_lists=["course__students"], user_fields=["course__instructor_id"]),
+    'profile': ItemQueryDesc(Profile, "account.view_profile", user_fields=["user"]),
 
 }
 
 
 def get_all(req, query_dict):
     if not req.user.is_authenticated:
-        return HttpResponse('{"message": "You are not logged in"}', status=401)
+        return HttpResponse('{"message": "You are not logged in"}', status=HTTPStatus.UNAUTHORIZED)
 
     item_type = query_dict.get('item_type')
 
     if(item_type == None):
-        return HttpResponse('{"message": "Item type not specified"}', status=400)
+        return HttpResponse('{"message": "Item type not specified"}', status=HTTPStatus.BAD_REQUEST)
 
-    item_model = ITEM_MODELS.get(item_type)['model']
+    item_query_desc = ITEM_MODELS.get(item_type)
 
-    if(item_model == None):
-        return HttpResponse('{"message":"Item type not recognized"}', status=404)
+    if item_query_desc["only_mine"]:
+        return HttpResponse('{"message": "You can only get your own data from this list"}', status=HTTPStatus.FORBIDDEN)
+
+    if(item_query_desc == None):
+        return HttpResponse('{"message":"Item type not recognized"}', status=HTTPStatus.NOT_FOUND)
+
+    item_model = item_query_desc['model']
 
     if not req.user.has_perm(ITEM_MODELS.get(item_type)['permission']):
-        return HttpResponse('{"message": "You do not have permission to view that item"}', status=403)
+        return HttpResponse('{"message": "You do not have permission to view that item"}', status=HTTPStatus.FORBIDDEN)
 
 
     items = item_model.objects.all()
@@ -62,8 +78,48 @@ def get_all(req, query_dict):
 
     return HttpResponse(dumps(data, cls=DatabaseEncoder))
 
+def get_mine(req, query_dict):
+    if not req.user.is_authenticated:
+        return HttpResponse('{"message": "You are not logged in"}', status=HTTPStatus.UNAUTHORIZED)
+
+    item_type = query_dict.get('item_type')
+
+    if(item_type == None):
+        return HttpResponse('{"message": "Item type not specified"}', status=HTTPStatus.BAD_REQUEST)
+
+    item_query_desc = ITEM_MODELS.get(item_type)
+
+    if(item_query_desc == None):
+        return HttpResponse('{"message":"Item type not recognized"}', status=HTTPStatus.NOT_FOUND)
+
+    item_model = item_query_desc['model']
+
+    if not req.user.has_perm(ITEM_MODELS.get(item_type)['permission']):
+        return HttpResponse('{"message": "You do not have permission to view that item"}', status=HTTPStatus.FORBIDDEN)
+
+    search_params = Q()
+
+
+    for field in item_query_desc["user_fields"]:
+        keyword = {}
+        keyword[field] = req.user.id
+        search_params |= Q(**keyword)
+    for field in item_query_desc["user_lists"]:
+        keyword = {}
+        keyword['{0}__id'.format(field)] = req.user.id
+        search_params |= Q(**keyword)
+        
+    items = item_model.objects.filter(search_params)
+
+    data = {
+        'items': queryset_to_list(items)
+    }
+
+    return HttpResponse(dumps(data, cls=DatabaseEncoder))
+
 QUERY_COMMANDS = {
-    'get_all': get_all 
+    'get_all': get_all,
+    'get_mine': get_mine,
 }
 
 # Create your views here.
@@ -73,12 +129,12 @@ def basic_query(req):
     command = query_dict.get('command')
 
     if(command == None):
-        return HttpResponse('{"message":"Command not specified"}', status=400)
+        return HttpResponse('{"message":"Command not specified"}', status=HTTPStatus.BAD_REQUEST)
 
     command_handler = QUERY_COMMANDS.get(command)
 
     if(command_handler == None):
-        return HttpResponse('{"message":"Unrecognized command"}', status=400)
+        return HttpResponse('{"message":"Unrecognized command"}', status=HTTPStatus.BAD_REQUEST)
 
     return command_handler(req, query_dict)
 
