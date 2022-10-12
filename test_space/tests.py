c87312e14e92aed9ade52e3d48ff028717a9b80a
Griffin <GriffinGould@mail.weber.edu>
2022-10-12 09:12:42 -0600

    Added create course test

--- test_space/tests.py
+++ test_space/tests.py
@@ -1,3 +1,55 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.test import Client
+from account.models import Profile
+from course_management.models import Course
+import unittest
 
 # Create your tests here.
+
+class TestClass(TestCase):
+    # Set up for the run
+    def setUp(self):
+
+        pass
+
+    # Test add class
+    def test_addclass(self):
+        # Create a test user
+        user = User.objects.create_user('testprofessor', 'prof@gmail.com', 'asdfasdfasdf')
+        Profile.user = user
+        c = Client()
+
+        # Login the user
+        success = c.login(username='testprofessor', password='asdfasdfasdf')
+
+        # Check if login was successful
+        self.assertTrue(success)
+
+        # Create the course
+        response = c.post('/courses/addCourse/', {'department': 'CS',
+                                                  'course_num': 4000,
+                                                  'course_name': 'Test course',
+                                                  'meeting_days': 'T,Th',
+                                                  'meeting_start_time': '12:00',
+                                                  'meeting_end_time': '12:30',
+                                                  'meeting_location': 'Building 100',
+                                                  'credit_hours': 4})
+
+        # Status code should be 302 for redirect
+        self.assertTrue(response.status_code == 302)
+
+        course_list = Course.objects.filter(instructor=user)
+
+        # Check if the course was created
+        self.assertEqual(course_list[0].course_num, 4000)
+
+        pass
+
+    # Clean up after the test
+    def tearDown(selfself):
+
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
