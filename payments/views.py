8328e14cec6bc25c957ab11651fa2218186a2454
Artem <arty.nar00@gmail.com>
2022-10-11 19:28:29 -0600

    Added tuition functionality and model

--- payments/views.py
+++ payments/views.py
@@ -1,26 +1,17 @@
 from django.shortcuts import render
 from course_management.models import Course
 from django.contrib.auth.models import Group
-
-# A helper function to calculate the balance of a student
-def calculateBalance(course_list):
-    # Total credit hours
-    chrs = 0
-
-    # Adds all credit hours together 
-    for course in course_list:
-        chrs += course.credit_hours
-
-    return chrs*100
+from .models import Tuition
 
 # Create your views here.
 def tuition(request):
-    group = Group.objects.get(name='Student')
-    print(group)
     # A precaution, in case student/course relationship does not exist
+    balance = Tuition.objects.get(user=request.user).balance
+
     try:        
         course_list = request.user.courses.all()
-        balance = calculateBalance(course_list)
+
+
         return render(request, 'payments/tuition_page.html', {'course_list' : course_list, 'balance' : balance})
     except:  
         return render(request, 'payments/tuition_page.html', {'course_list' : {}})
