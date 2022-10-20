3cc71a8c52d630ae3710658aac9681a1fc1e0c04
Artem <arty.nar00@gmail.com>
2022-10-20 09:40:21 -0600

    Added the test. Does not pass yet

--- course_management/tests.py
+++ course_management/tests.py
@@ -9,13 +9,148 @@ from payments.models import Tuition
 
 # Create your tests here.
 
-class TestMakeCourse(TestCase):
+# class TestMakeCourse(TestCase):
+#     # Set up for the run
+#     user = None
+#     c = Client()
+#     def setUp(self):
+#         # Create a test user
+#         user = User.objects.create_user('testprofessor', 'prof@gmail.com', 'asdfasdfasdf')
+#         Profile.user = user
+
+#         # Login the user
+#         success = self.c.login(username='testprofessor', password='asdfasdfasdf')
+
+#         # Check if login was successful
+#         self.assertTrue(success, msg='Error: Login failed.')
+
+#     # Test add class
+#     def test_addclass(self):
+
+#         # Create the course
+#         response = self.c.post('/courses/addCourse/', {'department': 'CS',
+#                                                   'course_num': 4000,
+#                                                   'course_name': 'Test course',
+#                                                   'meeting_days': 'T,Th',
+#                                                   'meeting_start_time': '12:00',
+#                                                   'meeting_end_time': '12:30',
+#                                                   'meeting_location': 'Building 100',
+#                                                   'credit_hours': 4})
+
+#         # Status code should be 302 for redirect
+#         self.assertTrue(response.status_code == 302, msg='Error: Post failed to return redirection status.')
+
+#         course_list = Course.objects.filter(course_num=4000)
+
+#         # Check if the course was created
+#         self.assertEqual(course_list[0].course_num, 4000, msg='Error: Failed to create a course.')
+
+#     # Clean up after the test
+#     def tearDown(selfself):
+#         # Place code here you want to run after the test
+#         pass
+
+# class StudentCanRegisterForCourseTest(TestCase):
+#     # Set up for the run
+#     user = None
+#     c = Client()
+#     def setUp(self):
+#         self.user = User.objects.create_user('registering_student', 'student@stu.dent', 'asdfasdfasdf')
+#         Tuition.objects.create(user=self.user)
+#         professor = User.objects.create_user('professor_test', 'test@pro.fessor', 'asdfasdfasdf')
+#         Course.objects.create(department='THING', course_num=2210, course_name='Testing things',
+#                               instructor=professor, meeting_days='M,T,W', meeting_start_time='12:00',
+#                               meeting_end_time='1:00', meeting_location='where ever, lol', credit_hours=3)
+
+#         Profile.user = self.user
+
+#         # Login the user
+#         success = self.c.login(username='registering_student', password='asdfasdfasdf')
+        
+#     def test_student_register(self):
+#         # get objects created in setup
+#         course = Course.objects.filter(department='THING', course_num=2210).first()
+
+#         # create url for registration
+#         url = '/courses/registration/register/' + str(course.id)
+
+#         # register for the course
+#         response = self.c.post(url)
+
+#         # Status code should be 302 for redirect
+#         self.assertTrue(response.status_code == 302, msg='Error: Post failed to return redirection status. Instead returned '+str(response.status_code))
+
+#         # Check if the student is in the course
+#         self.assertTrue(course in self.user.courses.all(), msg='Error: Failed to register for course.')
+
+#     # Clean up after the test
+#     def tearDown(self):
+#         # Place code here you want to run after the test
+#         pass
+        
+# class StudentSubmitTextAssignmentTest(TestCase):
+#     # Set up for the run
+#     user = None
+#     c = Client()
+#     def setUp(self):
+#         # Create test objects
+#         user = User.objects.create_user('teststudent', 'student@gmail.com', 'asdfasdfasdf')
+#         professor = User.objects.create_user('testprofessor', 'prof@gmail.com', 'asdfasdfasdf')
+#         course = Course.objects.create(department='CS', course_num=4000, course_name='Test course',
+#                                        instructor=professor, meeting_days='T,Th', meeting_start_time='12:00',
+#                                        meeting_end_time='12:30', meeting_location='Building 100', credit_hours=4)
+#         assignment = Assignment.objects.create(course=course, title='Test Assignment',
+#                                                description='This is the test assignment',
+#                                                due_date='2022-12-31 23:59:00', points=100, type='t')
+
+#         Profile.user = user
+
+#         # Login the user
+#         success = self.c.login(username='teststudent', password='asdfasdfasdf')
+
+#         # Check if login was successful
+#         self.assertTrue(success, msg='Error: Login failed.')
+
+#     # Test add class
+#     def test_student_submit_text_assignment(self):
+
+#         # get objects created in setup
+#         user = User.objects.filter(username='teststudent').first()
+#         course = Course.objects.filter(course_num=4000).first()
+#         assignment = Assignment.objects.filter(title='Test Assignment').first()
+
+#         # create url for submission post
+#         url = '/courses/' + str(course.id) + '/' + str(assignment.id)
+
+#         # Create the submission
+#         response = self.c.post(url, {'text': 'This is the test text submission'})
+
+#         # Status code should be 302 for redirect
+#         self.assertTrue(response.status_code == 302, msg='Error: Post failed to return redirection status.')
+
+#         submissions = TextSubmission.objects.filter(student=user, assignment=assignment)
+
+#         # Check if the course was created
+#         self.assertEqual(submissions[0].text, 'This is the test text submission', msg='Error: Failed to create submission.')
+
+#     # Clean up after the test
+#     def tearDown(self):
+#         # Place code here you want to run after the test
+#         pass
+        
+# if __name__ == '__main__':
+#     unittest.main()
+
+class CreateAssignmentTest(TestCase):
     # Set up for the run
     user = None
     c = Client()
     def setUp(self):
         # Create a test user
         user = User.objects.create_user('testprofessor', 'prof@gmail.com', 'asdfasdfasdf')
+        course = Course.objects.create(department='CS', course_num=4000, course_name='Test course',
+                                        instructor=user, meeting_days='T,Th', meeting_start_time='12:00',
+                                        meeting_end_time='12:30', meeting_location='Building 100', credit_hours=4)
         Profile.user = user
 
         # Login the user
@@ -25,118 +160,31 @@ class TestMakeCourse(TestCase):
         self.assertTrue(success, msg='Error: Login failed.')
 
     # Test add class
-    def test_addclass(self):
+    def test_create_assignment(self):
+        # get objects created in setup
+        course = Course.objects.filter(course_num=4000).first()
 
-        # Create the course
-        response = self.c.post('/courses/addCourse/', {'department': 'CS',
-                                                  'course_num': 4000,
-                                                  'course_name': 'Test course',
-                                                  'meeting_days': 'T,Th',
-                                                  'meeting_start_time': '12:00',
-                                                  'meeting_end_time': '12:30',
-                                                  'meeting_location': 'Building 100',
-                                                  'credit_hours': 4})
+        print(course.id)
+        # create url for submission post
+        url = '/courses/' + str(course.id) + '/addAssignment'
+
+        # Create the submission
+        response = self.c.post(url, {'title': 'UnitTestAssign', 
+                                    'description': 'Test Description',
+                                    'due_date': '2022-09-02 10:10:00',
+                                    'points': 100,
+                                    'type': 't'})
 
         # Status code should be 302 for redirect
+        print(response.status_code)
         self.assertTrue(response.status_code == 302, msg='Error: Post failed to return redirection status.')
 
-        course_list = Course.objects.filter(course_num=4000)
+        assignments_list = Assignment.objects.filter(title='UnitTestAssign')
 
         # Check if the course was created
-        self.assertEqual(course_list[0].course_num, 4000, msg='Error: Failed to create a course.')
+        self.assertEqual(assignments_list[0].title, 'UnitTestAssign', msg='Error: Failed to create an assignment.')
 
     # Clean up after the test
     def tearDown(selfself):
         # Place code here you want to run after the test
         pass
-
-class StudentCanRegisterForCourseTest(TestCase):
-    # Set up for the run
-    user = None
-    c = Client()
-    def setUp(self):
-        self.user = User.objects.create_user('registering_student', 'student@stu.dent', 'asdfasdfasdf')
-        Tuition.objects.create(user=self.user)
-        professor = User.objects.create_user('professor_test', 'test@pro.fessor', 'asdfasdfasdf')
-        Course.objects.create(department='THING', course_num=2210, course_name='Testing things',
-                              instructor=professor, meeting_days='M,T,W', meeting_start_time='12:00',
-                              meeting_end_time='1:00', meeting_location='where ever, lol', credit_hours=3)
-
-        Profile.user = self.user
-
-        # Login the user
-        success = self.c.login(username='registering_student', password='asdfasdfasdf')
-        
-    def test_student_register(self):
-        # get objects created in setup
-        course = Course.objects.filter(department='THING', course_num=2210).first()
-
-        # create url for registration
-        url = '/courses/registration/register/' + str(course.id)
-
-        # register for the course
-        response = self.c.post(url)
-
-        # Status code should be 302 for redirect
-        self.assertTrue(response.status_code == 302, msg='Error: Post failed to return redirection status. Instead returned '+str(response.status_code))
-
-        # Check if the student is in the course
-        self.assertTrue(course in self.user.courses.all(), msg='Error: Failed to register for course.')
-
-    # Clean up after the test
-    def tearDown(self):
-        # Place code here you want to run after the test
-        pass
-        
-class StudentSubmitTextAssignmentTest(TestCase):
-    # Set up for the run
-    user = None
-    c = Client()
-    def setUp(self):
-        # Create test objects
-        user = User.objects.create_user('teststudent', 'student@gmail.com', 'asdfasdfasdf')
-        professor = User.objects.create_user('testprofessor', 'prof@gmail.com', 'asdfasdfasdf')
-        course = Course.objects.create(department='CS', course_num=4000, course_name='Test course',
-                                       instructor=professor, meeting_days='T,Th', meeting_start_time='12:00',
-                                       meeting_end_time='12:30', meeting_location='Building 100', credit_hours=4)
-        assignment = Assignment.objects.create(course=course, title='Test Assignment',
-                                               description='This is the test assignment',
-                                               due_date='2022-12-31 23:59:00', points=100, type='t')
-
-        Profile.user = user
-
-        # Login the user
-        success = self.c.login(username='teststudent', password='asdfasdfasdf')
-
-        # Check if login was successful
-        self.assertTrue(success, msg='Error: Login failed.')
-
-    # Test add class
-    def test_student_submit_text_assignment(self):
-
-        # get objects created in setup
-        user = User.objects.filter(username='teststudent').first()
-        course = Course.objects.filter(course_num=4000).first()
-        assignment = Assignment.objects.filter(title='Test Assignment').first()
-
-        # create url for submission post
-        url = '/courses/' + str(course.id) + '/' + str(assignment.id)
-
-        # Create the submission
-        response = self.c.post(url, {'text': 'This is the test text submission'})
-
-        # Status code should be 302 for redirect
-        self.assertTrue(response.status_code == 302, msg='Error: Post failed to return redirection status.')
-
-        submissions = TextSubmission.objects.filter(student=user, assignment=assignment)
-
-        # Check if the course was created
-        self.assertEqual(submissions[0].text, 'This is the test text submission', msg='Error: Failed to create submission.')
-
-    # Clean up after the test
-    def tearDown(self):
-        # Place code here you want to run after the test
-        pass
-        
-if __name__ == '__main__':
-    unittest.main()
