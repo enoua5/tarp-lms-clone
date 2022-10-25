dc127321109fc02d87f218abef85a40a62d9dd56
Abigail (Abby) Goodwin <abby.goodwin@outlook.com>
2022-10-25 13:37:20 -0600

    Swapping to Chrome for the default UI testing browser.

--- authentication/tests.py
+++ authentication/tests.py
@@ -4,6 +4,9 @@ from django.test import LiveServerTestCase
 
 # Selenium Imports
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
@@ -12,23 +15,29 @@ from selenium.webdriver.common.keys import Keys
     @details Allows the developer to verify that a user is able to log in.
 '''
 class LoginFormTest(LiveServerTestCase):
-        
+    '''!
+        @brief The below will install the correct browser driver (default to Chrome)
+            for our UI tests.
+    '''
+    service = Service(executable_path=ChromeDriverManager().install())
+    
     def test_loginform(self):
-        selenium = webdriver.Firefox()
-        #Choose your url to visit
+        selenium = webdriver.Chrome(service=self.service)
+        # Give Selenium the URL to go to.
         selenium.get('http://localhost:8000/login/?next=/dashboard/')
 
-        #find the elements you need to submit form
+        # Get the elements that we'll be interacting with.
         username_field = selenium.find_element(By.ID, 'username')
         password_field = selenium.find_element(By.ID, 'password')
         login_btn = selenium.find_element(By.XPATH, "//input[@type='submit'][@value='Log In']")
 
-        #populate the form with data
+        # Populate the form with user input.
         username_field.send_keys('professor')
         password_field.send_keys('asdfasdfasdf')
 
-        #submit form
+        # Click login
         login_btn.click()
 
-        #check result; page source looks at entire html document
+        # Check that we were redirected to the dashboard and we see "Professor Bean"
         assert 'Professor Bean' in selenium.page_source
+        assert 
