dcb872cc54bba434f641cfd683f42d30d56f2427
Abigail (Abby) Goodwin <abby.goodwin@outlook.com>
2022-10-25 13:20:53 -0600

    Added selenium requirement; added first UI test.

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,31 @@
+# Django Imports
 from django.test import TestCase
+from django.test import LiveServerTestCase
 
-# Create your tests here.
+# Selenium Imports
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+# Test Cases
+class LoginFormTest(LiveServerTestCase):
+
+  def loginform(self):
+    selenium = webdriver.Firefox()
+    #Choose your url to visit
+    selenium.get('http://localhost:8000/login/?next=/dashboard/')
+    
+    #find the elements you need to submit form
+    username_field = selenium.find_element(By.ID, 'username')
+    password_field = selenium.find_element(By.ID, 'password')
+    login_btn = selenium.find_element(By.XPATH, "//input[@type='button'][@value='Log In']")
+
+    #populate the form with data
+    username_field.send_keys('professor')
+    password_field.send_keys('asdfasdfasdf')
+
+    #submit form
+    login_btn.click()
+
+    #check result; page source looks at entire html document
+    assert 'Professor Bean' in selenium.page_source
