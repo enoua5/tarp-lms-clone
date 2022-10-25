8950ad51f73387d19b220f674f37d0e9ea9eeb46
Abigail (Abby) Goodwin <abby.goodwin@outlook.com>
2022-10-25 13:30:35 -0600

    Completed login form UI test.

--- authentication/tests.py
+++ authentication/tests.py
@@ -7,25 +7,28 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-# Test Cases
+'''!
+    @brief Test suite for the Login form.
+    @details Allows the developer to verify that a user is able to log in.
+'''
 class LoginFormTest(LiveServerTestCase):
+        
+    def test_loginform(self):
+        selenium = webdriver.Firefox()
+        #Choose your url to visit
+        selenium.get('http://localhost:8000/login/?next=/dashboard/')
 
-  def loginform(self):
-    selenium = webdriver.Firefox()
-    #Choose your url to visit
-    selenium.get('http://localhost:8000/login/?next=/dashboard/')
-    
-    #find the elements you need to submit form
-    username_field = selenium.find_element(By.ID, 'username')
-    password_field = selenium.find_element(By.ID, 'password')
-    login_btn = selenium.find_element(By.XPATH, "//input[@type='button'][@value='Log In']")
+        #find the elements you need to submit form
+        username_field = selenium.find_element(By.ID, 'username')
+        password_field = selenium.find_element(By.ID, 'password')
+        login_btn = selenium.find_element(By.XPATH, "//input[@type='submit'][@value='Log In']")
 
-    #populate the form with data
-    username_field.send_keys('professor')
-    password_field.send_keys('asdfasdfasdf')
+        #populate the form with data
+        username_field.send_keys('professor')
+        password_field.send_keys('asdfasdfasdf')
 
-    #submit form
-    login_btn.click()
+        #submit form
+        login_btn.click()
 
-    #check result; page source looks at entire html document
-    assert 'Professor Bean' in selenium.page_source
+        #check result; page source looks at entire html document
+        assert 'Professor Bean' in selenium.page_source
