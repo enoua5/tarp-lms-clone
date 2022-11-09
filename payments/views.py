65e40c9c8a530e553071af04cfa94d4e4029fd57
Abigail (Abby) Goodwin <abby.goodwin@outlook.com>
2022-11-08 18:33:52 -0700

    Styling tuition page.

--- payments/views.py
+++ payments/views.py
@@ -8,9 +8,29 @@ def tuition(request):
     # A precaution, in case student/course relationship does not exist
     try:        
         course_list = request.user.courses.all()
-        balance = Tuition.objects.get(user=request.user).balance
+        balance = float(Tuition.objects.get(user=request.user).balance)
+        total = getSemesterTotal(request.user)
+        amt_paid = total - balance
 
-        return render(request, 'payments/tuition_page.html', {'course_list' : course_list, 'balance' : balance})
+        return render(request, 'payments/tuition_page.html', {'course_list': course_list,
+                                                              'balance': f"{balance:0.2f}",
+                                                              'total': f"{total:0.2f}",
+                                                              'paid': f"{amt_paid:0.2f}"})
     except:  
         return render(request, 'payments/tuition_page.html', {'course_list' : {}})
 
+
+## Supporting Functions
+'''!
+    @brief Calculates the total cost of the semester for the student.
+    @param student The student object.
+    @return total The total cost of the semester for the student (before payment).
+'''
+def getSemesterTotal(student):
+    course_list = student.courses.all()
+    total = 0.0
+    
+    for course in course_list:
+        total += course.credit_hours * 100.0
+        
+    return total
